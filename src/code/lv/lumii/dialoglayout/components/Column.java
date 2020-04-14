fce09866320c081c667e8ffc5ee1195aef4b82d1
Sergejs Kozlovičs <sergejs.kozlovics@gmail.com>
2020-04-15 00:15:40 +0300

    Column.java + js binary

--- src/code/lv/lumii/dialoglayout/components/Column.java
+++ src/code/lv/lumii/dialoglayout/components/Column.java
@@ -1,3 +1,22 @@
+/*
+*
+* Copyright (c) 2013-2020 Institute of Mathematics and Computer Science, University of Latvia (IMCS UL). 
+* Relevant scientific papers:
+*  - S. Kozlovics. A Dialog Engine Metamodel for the Transformation-Driven Architecture. In: Scientific Papers, University of Latvia. vol. 756, pp. 151-170 (2010)
+*  - S. Kozlovics. Calculating The Layout For Dialog Windows Specified As Models. In: Scientific Papers, University of Latvia. vol. 787, pp. 106-124 (2012)
+
+* This file is part of layoutengine
+*
+* You can redistribute it and/or modify it under the terms of the GNU General Public License 
+* as published by the Free Software Foundation, either version 2 of the License,
+* or (at your option) any later version.
+*
+* This file is also subject to the "Classpath" exception as mentioned in
+* the COPYING file that accompanied this code.
+*
+* You should have received a copy of the GNU General Public License along with layoutengine. If not, see http://www.gnu.org/licenses/.
+*
+*/
 package lv.lumii.dialoglayout.components;
 
 import lv.lumii.layoutengine.funcmin.ExtendedQuadraticOptimizer;
@@ -10,29 +29,20 @@ public class Column extends Component {
 	protected Column prevColumn = null;	
 	
 	public void writeGravity(ExtendedQuadraticOptimizer eqo,double coeff) {
-		//get children count
-		int comp_count=children.size();
 		//stores references to first and last child
 		Component first=null;
 		Component last=null;
 		
-		double newCoeff = coeff/2/(comp_count+1);
-		
+		double newCoeff = Layout.getChildGravity(coeff, children.size());		
 		
 		//for every child
 		for (Component child:getChildren()) {
 			ComponentBounds childBounds=child.getComponentBounds();
 			
 			//add gravity to form's left and right borders
-			eqo.addLinearDifference(bounds.xl, childBounds.xl, newCoeff);
-			eqo.addLinearDifference(childBounds.xr, bounds.xr, newCoeff);
-			
-			//eqo.addInequality(bounds.xl, childBounds.xl, 0);
-			//eqo.addInequality(childBounds.xr, bounds.xr, 0);
-					
-			//eqo.addReducibleInequality(bounds.xl, childBounds.xl, 0, -Layout.INFINITY);
-			//eqo.addReducibleInequality(childBounds.xr, bounds.xr, 0, -Layout.INFINITY);
-			
+			eqo.addLinearDifference(bounds.xl, childBounds.xl, newCoeff*Layout.GRAVITY_WEIGHT_ADJUSTMENT);
+			eqo.addLinearDifference(childBounds.xr, bounds.xr, newCoeff*Layout.GRAVITY_WEIGHT_ADJUSTMENT);
+						
 			//add child's inner gravity, update the gravity coefficient so that it wouldn't affect the form itself
 			child.writeGravity(eqo, newCoeff);
 			
@@ -44,11 +54,9 @@ public class Column extends Component {
 		
 		//add gravity to form's top and bottom borders
 		if (first!=null)
-			eqo.addEquality(first.getComponentBounds().xt, bounds.xt, 0);
-			//eqo.addLinearDifference(bounds.xt, first.getComponentBounds().xt, coeff);
+			eqo.addLinearDifference(bounds.xt, first.getComponentBounds().xt, coeff);
 		if (last!=null)
-			eqo.addEquality(bounds.xb, last.getComponentBounds().xb, 0);
-			//eqo.addLinearDifference(last.getComponentBounds().xb, bounds.xb, coeff);
+			eqo.addLinearDifference(last.getComponentBounds().xb, bounds.xb, coeff);
 	}
 	
 	public void writeConstraints(ExtendedQuadraticOptimizer eqo) {
@@ -77,8 +85,11 @@ public class Column extends Component {
 			if (prevColumnI<prevColumnChildren.length) {
 				Component pChild = prevColumnChildren[prevColumnI];
 				ComponentBounds pChildBounds = pChild.getComponentBounds();
-				eqo.addEquality(childBounds.xt, pChildBounds.xt, 0);
-				eqo.addEquality(childBounds.xb, pChildBounds.xb, 0);
+				
+				
+				eqo.addEquality(pChildBounds.xt, childBounds.xt, childBounds.topM-pChildBounds.topM);
+				eqo.addEquality(childBounds.xb, pChildBounds.xb, childBounds.bottomM-pChildBounds.bottomM);
+				
 				prevColumnI++;
 			}
 			
@@ -109,13 +120,16 @@ public class Column extends Component {
 		
 		if (firstChild!=null) {
 			//consider vertical padding and vertical alignment 
-			if (firstChild.getClass()!=Row.class && bounds.verAlign.equals("TOP"))
+			if (bounds.verAlign.equals("TOP")) {
 				eqo.addEquality(bounds.xt, firstChild.getComponentBounds().xt,bounds.topP+firstChild.getComponentBounds().topM);
-			else
+			}
+			else {
 				eqo.addInequality(bounds.xt, firstChild.getComponentBounds().xt,bounds.topP+firstChild.getComponentBounds().topM);
+			}
 			
-			if (firstChild.getClass()!=Row.class && bounds.verAlign.equals("BOTTOM"))
+			if (bounds.verAlign.equals("BOTTOM")) {
 				eqo.addEquality(lastChild.getComponentBounds().xb,bounds.xb,bounds.bottomP+lastChild.getComponentBounds().bottomM);
+			}
 			else
 				eqo.addInequality(lastChild.getComponentBounds().xb,bounds.xb,bounds.bottomP+lastChild.getComponentBounds().bottomM);
 			
